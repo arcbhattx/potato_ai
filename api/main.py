0260cb29008c1ba3b6835d4d101a1c8683644ec9
arcbhattx <arcbh11@gmail.com>
2025-07-11 15:57:18 -0700

    added more

--- api/main.py
+++ api/main.py
@@ -1,11 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
 app = FastAPI()
+
+
+MODEL = tf.keras.models.load_model("../models/1")
 
 
 @app.get('/ping')
 async def ping():
     return "Hello I am alive!!"
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    prediction = MODEL.predict(img_batch)
+    pass
+
+
 
 
 if __name__ == "__main__":
