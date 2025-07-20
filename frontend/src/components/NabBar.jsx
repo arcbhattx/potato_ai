cb55eef86d1789d1d50f4994a02223b1919890ec
arcbhattx <arcbh11@gmail.com>
2025-07-20 16:00:25 -0700

    frontend changes

---
+++ frontend/src/components/NabBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavBar() {
+  const navbar_elements = ['Home', 'About', 'Settings'];
+
+  return (
+    <div className="bg-black text-white px-8 py-4 flex justify-between items-center shadow-md">
+      <p className='text-white'> Black box </p>
+      
+    </div>
+  );
+}
+
+export default NavBar;
